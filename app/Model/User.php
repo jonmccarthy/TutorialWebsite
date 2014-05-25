9a2330577b449872c4b23f76f6a12305593c5fd0
jonmccarthy <jonathan.mccarthy.ire@gmail.com>
2014-05-26 00:11:12 +0100

    DB Script

--- app/Model/User.php
+++ app/Model/User.php
@@ -1,6 +1,7 @@
 <?php
 // app/Model/User.php
 App::uses('AppModel', 'Model');
+App::uses('SimplePasswordHasher', 'Controller/Component/Auth');
 
 class User extends AppModel {
     public $validate = array(
@@ -24,5 +25,15 @@ class User extends AppModel {
             )
         )
     );
+    
+    public function beforeSave($options = array()) {
+		if (isset($this->data[$this->alias]['password'])) {
+	        $passwordHasher = new SimplePasswordHasher();
+	        $this->data[$this->alias]['password'] = $passwordHasher->hash(
+	            $this->data[$this->alias]['password']
+	        );
+	    }
+	    return true;
+	}
 }
 ?>
